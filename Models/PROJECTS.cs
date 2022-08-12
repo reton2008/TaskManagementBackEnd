c4a60903f0377f9a747882df16f6957f721ab541
ATIKUR RAHAMAN <reton2008@gmail.com>
2022-08-13 04:20:36 +0600

    added README.md

---
+++ Models/PROJECTS.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace Models
+{
+    [Table("PROJECTS")]
+    public class PROJECTS
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; } 
+
+        [Required]
+        public int PROJECT_NAME { get; set; }
+
+        [Required]
+        public int PROJECT_MANAGER { get; set; }
+
+        [ForeignKey("TEAM_MANAGER_ID")]
+        public EMPLOYEE TEAM_MANAGER_Id { get; set; }
+
+        [Required]
+        public int PROJECT_TEAM { get; set; }
+
+        [ForeignKey("PROJECT_TEAM_ID")]
+        public TEAMS PROJECT_TEAM_ID { get; set; }
+
+        [Required]
+        public DateOnly? START_DATE { get; set; }
+
+        [Required]
+        public DateOnly? END_DATE { get; set; }
+
+        public int? MAKE_BY { get; set; }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public DateTime? CREATED_AT { get; set; }
+
+        [System.ComponentModel.DefaultValue("A")]
+        public string? STATUS { get; set; }
+    }
+}
---
+++ Models/TEAMS_DETAILS.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace Models
+{
+    [Table("TEAMS_DETAILS")]
+    public class TEAMS_DETAILS
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+
+        [Required]
+        public int TEAM_ID{ get; set; }
+
+        [ForeignKey("TEAM_DETAILS_ID")]
+        public TEAMS TEAM_DETAILS_ID { get; set; }
+
+
+        [Required]
+        public int? TEAMS_EMPLOYEE { get; set; }
+
+        [ForeignKey("TEAMS_EMPLOYEE_ID")]
+        public EMPLOYEE TEAMS_EMPLOYEE_ID { get; set; }
+
+        public int? MAKE_BY { get; set; }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public DateTime? CREATED_AT { get; set; }
+
+        [System.ComponentModel.DefaultValue("A")]
+        public string? STATUS { get; set; }
+
+
+    }
+}
